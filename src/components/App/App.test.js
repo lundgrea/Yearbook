a2a48d7468103a3346b3de62535c2ea792d6a681
Alyssa Lundgren <lundgrea@gmail.com>
2019-08-17 15:20:51 -0600

    add app snapshot test

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,5 +1,11 @@
 import React from 'react';
+import { shallow } from 'enzyme';
 import App from './App';
 
-it('renders without crashing', () => {
-});
+describe('App', () => {
+  it('should match the snapshot with all data passed in correctly', () => {
+    const wrapper = shallow(<App
+    />)
+  expect(wrapper).toMatchSnapshot()
+  })
+})
